70282c46a12b8b10684ed93cd910a9de3f50461f
Nikolaas <nikotomad@gmail.com>
2018-11-27 13:46:23 +0100

    Navbar refactor

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import './styles.css';
 
+const navigationTabs = ['current', 'forecast', 'find me'];
+
 class Navbar extends Component {
+        state = { 
+            activeTab: 'current' 
+        }
+
+    changeTabOnClick = (clickedTab) => {
+        this.setState({ 
+            activeTab: clickedTab
+        })
+    }
+
     render() {
         return (
             <div className="Navbar">
-                <div className="Navbar__tab">Current</div>
-                <div className="Navbar__tab active">Forecast</div>
-                <div className="Navbar__tab">Find me</div>
+                { navigationTabs.map((tab, i) => <button key={i} className={"Navbar__tab" + (this.state.activeTab === tab ? ' active' : '')}> { tab } </button>) }
             </div>
         )
     }
