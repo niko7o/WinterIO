31f965a26debbc20eb1c98e3585cf64e4dc93404
Nikoto <nikotomad@gmail.com>
2018-11-28 17:52:23 +0100

    Updates

---
+++ precache-manifest.78d807448e1ff5bc3d692f9236760182.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c498c8f681ec3b2fb17b3d800bbb6562",
+    "url": "/WinterIO/static/media/bold-webfont.c498c8f6.woff"
+  },
+  {
+    "revision": "8d8956d11f6d1aa7fb76bf43b7ae1527",
+    "url": "/WinterIO/static/media/bold-webfont.8d8956d1.woff2"
+  },
+  {
+    "revision": "42ae2a49d5a0cb0968b5",
+    "url": "/WinterIO/static/js/runtime~main.42ae2a49.js"
+  },
+  {
+    "revision": "98fdb9150a417864f68d",
+    "url": "/WinterIO/static/js/main.98fdb915.chunk.js"
+  },
+  {
+    "revision": "19abf5f330a4e919a6a3",
+    "url": "/WinterIO/static/js/1.19abf5f3.chunk.js"
+  },
+  {
+    "revision": "98fdb9150a417864f68d",
+    "url": "/WinterIO/static/css/main.36adcaf7.chunk.css"
+  },
+  {
+    "revision": "f7347d8e072e11e080ff3922c824884f",
+    "url": "/WinterIO/index.html"
+  }
+];
