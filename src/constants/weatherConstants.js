d679b82d9322f57a9a15488d70bfdf2a1dd8bce1
Nikolaas <nikotomad@gmail.com>
2018-12-03 11:20:44 +0100

    changed weather constants order

--- src/constants/weatherConstants.js
+++ src/constants/weatherConstants.js
@@ -4,9 +4,9 @@ export const CODE_ICONS = {
     '04d': '../../weather_icons/cloud.png', // broken clouds
     '02d': '../../weather_icons/cloud.png', // few clouds
     '02n': '../../weather_icons/cloud.png', // few clouds
+    '50d': '../../weather_icons/cloud.png', //fog
+    '50n': '../../weather_icons/cloud.png',  // mist
     '03n': '../../weather_icons/cloud.png', // scattered clouds
     '10n': '../../weather_icons/rain.gif', // light rain
     '10d': '../../weather_icons/rain.gif', // light rain
-    '50d': '../../weather_icons/cloud.png', //fog
-    '50n': '../../weather_icons/cloud.png',  // mist
 }
